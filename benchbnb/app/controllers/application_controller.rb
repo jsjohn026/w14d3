465fa8a35210a38ae35da37363e48ea4ab464e98
Jasmine <jsjohn026@gmail.com>
2019-05-15 16:10:10 -0700

    update bench_bnb session actions

--- benchbnb/app/controllers/application_controller.rb
+++ benchbnb/app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user, :ensure_logged_in, :logged_in?
+
+  def login(user)
+    session[:session_token] = user.reset_session_token!
+    @current_user = user
+  end
+
+  def logout
+    session[:session_token] = nil
+    current_user.reset_session_token!
+    @current_user = nil
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def ensure_logged_in
+    redirect_to new_session_url unless logged_in?
+  end
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
 end
